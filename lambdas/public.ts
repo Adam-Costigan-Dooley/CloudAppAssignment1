180a4d6693310381a0c8521d89e5da629de97414
Adam-Costigan-Dooley <116007719+Adam-Costigan-Dooley@users.noreply.github.com>
2025-11-12 18:35:21 +0000

    Added App API endpoints.

---
+++ lambdas/public.ts
@@ -0,0 +1,8 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandlerV2 = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Unauthenticated access allowed" }),
+  };
+};
